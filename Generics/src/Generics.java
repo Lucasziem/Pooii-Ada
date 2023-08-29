1480bf241d72cc2f443acc0a55f635fc1240d716
Lucasziem <lucsziem@hotmail.com>
2023-08-28 21:40:46 -0300

    Início de Projeto Generics

---
+++ Generics/src/Generics.java
@@ -0,0 +1,12 @@
+public class Generics {
+
+    public static void Generics (String args) {
+        String a = "a";
+        String b = "b";
+        System.out.println("Maior entre a e b: " + maior(a, b));
+    }
+
+    private static String maior(String primeira, String segunda) {
+        return "";
+    }
+}
